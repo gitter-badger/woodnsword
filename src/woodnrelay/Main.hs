1850899034f906d896d21596c12cb687a5ed2c51
Carter Hinsley <carterhinsley@gmail.com>
2015-03-22 12:46:10 +0000

    (doc) Update copyrights, add Haddock module docs

--- src/woodnrelay/Main.hs
+++ src/woodnrelay/Main.hs
@@ -1,2 +1,9 @@
+{-|
+Module      : Main
+Description : Entry point for the woodnrelay executable
+Copyright   : (c) Carter Hinsley, 2015
+License     : MIT
+Maintainer  : carterhinsley@gmail.com
+-}
 module Main where
 
--- src/woodnserver/Main.hs
+++ src/woodnserver/Main.hs
@@ -1,2 +1,9 @@
+{-|
+Module      : Main
+Description : Entry point for the woodnserver executable
+Copyright   : (c) Carter Hinsley, 2015
+License     : MIT
+Maintainer  : carterhinsley@gmail.com
+-}
 module Main where
 
